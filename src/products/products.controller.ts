1d70a7cb8892cd24502684eb393c92f94e893fcf
JReque <jrequena9@gmail.com>
2025-01-06 18:11:51 +0100

    list products

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { ProductsService } from './products.service';
+import { Product } from './interfaces/product.interface';
 
 @Controller('products')
-export class ProductsController {}
+export class ProductsController {
+  constructor(private readonly productsService: ProductsService) {}
+
+  @Get()
+  findAll(): Product[] {
+    return this.productsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number): Product {
+    return this.productsService.findOne(id);
+  }
+}
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Product } from './interfaces/product.interface';
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+  private readonly products: Product[] = [
+    { id: 1, name: 'Product 1', description: 'Description 1', price: 100 },
+    { id: 2, name: 'Product 2', description: 'Description 2', price: 200 },
+  ];
+
+  findAll(): Product[] {
+    return this.products;
+  }
+
+  findOne(id: number): Product {
+    const product = this.products.find((prod) => prod.id === id);
+    if (!product) {
+      throw new NotFoundException(`Product with ID ${id} not found`);
+    }
+    return product;
+  }
+}
